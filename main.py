6b36518d7caf23b99dc607cac4d38f33ca98f511
Ok-SeonWoo <dhrtjsdn0086@naver.com>
2024-03-14 21:05:07 +0900

    옥선우 2022111978

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 student_id = int(input("학번을 입력하세요: "))
 student_name = input("이름을 입력하세요: ")
+student_major = input("학과를 입력하세요: ")
 
-print("학번:', student_id)
+print("학번:", student_id)
 print("이름:", student_name)
+print("학과:", student_major)
 
